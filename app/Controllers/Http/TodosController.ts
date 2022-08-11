4febaf964daf22551274bc41eb8a8f4ac3495bb7
Anwar Hossain <mahedianwar@gmail.com>
2022-08-11 23:19:16 +0600

    crud on Todos

--- app/Controllers/Http/TodosController.ts
+++ app/Controllers/Http/TodosController.ts
@@ -1,3 +1,35 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 
-export default class TodosController {}
+import Todo from 'App/Models/Todo';
+
+export default class TodosController {
+  public async index() {
+    return Todo.all();
+  }
+
+  public async store({ request, response }: HttpContextContract) {
+    const data = request.only(['title', 'description', 'completed']);
+    const todo = await Todo.create(data);
+    return response.status(201).created({ message: 'created!', todo });
+  }
+
+  public async show({ params, response }: HttpContextContract) {
+    const todo = await Todo.findOrFail(params.id);
+    return response.status(200).json(todo);
+  }
+
+  public async update({ params, request, response }: HttpContextContract) {
+    const todo = await Todo.findOrFail(params.id);
+    todo.title = request.input('title');
+    todo.description = request.input('description');
+    todo.completed = request.input('completed');
+    await todo.save();
+    return response.json(todo);
+  }
+
+  public async destroy({ params, response }: HttpContextContract) {
+    const todo = await Todo.findOrFail(params.id);
+    await todo.delete();
+    return response.status(200).noContent();
+  }
+}
